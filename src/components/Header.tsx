e6ed5d4bc8c2fa9eaa78946cf588b17432d48d04
parkjian7 <masichyun77@hanmail.net>
2024-11-07 19:51:56 +0900

    Feat: 불필요한 디자인 제거

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { FiAlignJustify } from "react-icons/fi";
 import { GoPerson  } from "react-icons/go";
 import { IoIosHome } from "react-icons/io";
 import Navbar from './Navbar'
@@ -73,19 +72,12 @@ const Header: React.FC = () => {
           handleMouseLeave();
         }}>
           <div className={`flex flex-row items-center justify-between`}>
-            <button className={`w-36 text-base flex flex-col items-center justify-center h-full transition-colors duration-300`}>
-              <FiAlignJustify size="24"/>
-              <span className="mt-1">카테고리</span>
-            </button>
-            <ul className={`flex items-center justify-center font-bold text-sm relative z-10 py-2`}>
-              <li className="relative px-4" id="resume" onMouseEnter={() => handleMouseOver("resume")}>
+            <ul className={`flex items-center justify-center font-bold text-sm relative z-10 py-2 pl-20`}>
+              <li className="relative px-8" id="resume" onMouseEnter={() => handleMouseOver("resume")}>
                 <strong className={`hover:underline hover:text-red-500`}>이력서</strong>
               </li>
               <li className="relative px-4" id="order" onMouseEnter={() => handleMouseOver("order")}>
                 <strong className={`hover:underline hover:text-red-500`}>주문</strong>
-              </li>
-              <li className="px-4" id="customerCenter" onMouseEnter={() => handleMouseOver("customerCenter")}>
-                <strong className={`hover:underline hover:text-red-500`}>고객센터</strong>
               </li>
             </ul>
             <div className="flex justify-end items-center h-full p-3">
