8a67e3fdf082815ab6917e534c6beb223d7f021e
parkjian7 <masichyun77@hanmail.net>
2024-11-13 21:10:01 +0900

    Fix: netlify에 토스 페이 success url시 page not found 되는 문제 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -9,5 +9,15 @@ export default defineConfig({
       'Access-Control-Allow-Origin': '*',
     },
     
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+    outDir: 'dist',
+    // _redirects 파일을 빌드 결과물에 포함
+    copyPublicDir: true,
   },
 })
